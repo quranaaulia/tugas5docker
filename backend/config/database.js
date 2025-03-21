090570caf1de697f40ba42c7d2d4b7f78b6f45e6
quranaaulia <162184412+quranaaulia@users.noreply.github.com>
2025-03-21 23:35:43 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,5 +3,5 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('notesnana', 'admin', 'nana', {
   host: '34.135.131.201',
   dialect: 'mysql',
-  port: 3000
+  port: 3306
 });
